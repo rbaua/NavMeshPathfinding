cca9d3eb026799e5e8cefc25009d5a86efdfdcd9
Rachel Haub <rachelhaub@gmail.com>
2020-10-15 21:22:16 -0700

    added to the BFS

--- P2_NavMeshPathfinding/P2/src/p2_pathfinder.py
+++ P2_NavMeshPathfinding/P2/src/p2_pathfinder.py
@@ -1,3 +1,4 @@
+import queue
 
 def find_path (source_point, destination_point, mesh):
 
@@ -11,17 +12,42 @@ def find_path (source_point, destination_point, mesh):
 
     Returns:
 
-        A path (list of points) from source_point to destination_point if exists
-        A list of boxes explored by the algorithm
+        A path (list of points) from source_point to destination_point if exists []
+        A list of boxes explored by the algorithm {}
     """
-
     path = []
     boxes = {}
-    for box in mesh['boxes']:
-        #print(box, 'box')
-        if box[0] < source_point[0] and box[1] > source_point[0] and box[2] < source_point[1] and box[3] > source_point[1]:
-            print('found source point')
-        if box[0] < destination_point[0] and box[1] > destination_point[0] and box[2] < destination_point[1] and box[3] > destination_point[1]:
-            print('found destination point')
-    print(mesh['adj'])
+    bfsqueue = queue.Queue(0)
+    bfsqueue.put([source_point, None])                  #[node, previous]
+    source_box = ()
+    dest_box = ()
+    for box in mesh['boxes']:                           #get the box for source_point and destination_point
+        if point_within_box(source_point, box):
+            source_box = box
+        if point_within_box(destination_point, box):
+            dest_box = box
+
+    while queue:                                        #breadth first search
+        current_node = bfsqueue.get()
+        if(current_node == destination_point):
+            print('gottem')
+            #return path
+        else:
+            for new in mesh['adj'][current_node]:
+                #if new.prev == none
+                    #new.prev = current_node
+                    #push new to queue
+                print()
     return path, boxes.keys()
+
+def point_within_box(point, box):                       #check if a point is within a given box
+    if box[0] < point[0] and box[1] > point[0] and box[2] < point[1] and box[3] > point[1]:
+        return True
+    else:
+        return False
+
+def box_of_point(mesh, point):
+    for box in mesh['boxes']:                           #get the box for a given point
+        if point_within_box(point, box):
+            return box
+    return False
